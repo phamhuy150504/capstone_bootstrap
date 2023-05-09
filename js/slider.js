2c79b4bac2da18d2e4188a5f1c6aed6aedead343
Huy  Phạm Gia <phamhuy15052004@gmail.com>
2023-05-09 10:44:39 +0700

    'capstone-bootstrap'

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.owl-one').owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      responsiveClass: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 1000,
+      autoplayHoverPause: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        480: {
+          items: 1,
+          nav: false
+        },
+        667: {
+          items: 1,
+          nav: true
+        },
+        1000: {
+          items: 1,
+          nav: true
+        }
+      }
+    })
+  })
